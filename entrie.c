32cad8aeb2bf52593e6f7620f7c4764526c73d6f
honoukossi <honoukossi24@gmail.com>
2020-02-15 20:38:33 +0100

    Modification du code sur la branche test

--- entrie.c
+++ entrie.c
@@ -3,6 +3,17 @@
 
 int main(void)
 {
-  printf("Toute chose a un debut");
+  int tab[20];
+  for (int i=0;i<20;i++){
+     tab[i] =i+1;
+  }
+  // affichons les valeurs du tableau
+
+  printf("Les valeurs du tableau sont: ");
+  for(int i=0;i<20;i++){
+     printf("%d, ",tab[i]);
+  }
+  printf("\n");
+
   return 0;
 }
